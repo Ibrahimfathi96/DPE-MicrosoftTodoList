9c5a288d964ca8a30b43f857136aacc98c3d00de
Ibrahim Fathi <117414627+Ibrahimfathi96@users.noreply.github.com>
2023-09-29 22:17:57 +0300

    [SERVER] get all Tasks / update/delete tasks not tested

--- server/routes/Tasks.js
+++ server/routes/Tasks.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const User = require("../models/UserModel");
 const tasksRouter = express.Router();
+
+//! ADD GROUP OF TASKLISTS
 tasksRouter.post("/api/addGroup", async (req, res) => {
   try {
     const { userId, name, backgroundColor } = req.body;
@@ -10,7 +12,6 @@ tasksRouter.post("/api/addGroup", async (req, res) => {
       return res.status(404).json({ msg: "User not found!" });
     }
     const todoList = {
-      // _id: new mongoose.Types.ObjectId(),
       name,
       backgroundColor
     };
@@ -21,6 +22,8 @@ tasksRouter.post("/api/addGroup", async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 });
+
+//! ADD TASK
 tasksRouter.post("/api/addTask", async (req, res) => {
   try {
     const { userId, todoId, todoTitle, todoDesc } = req.body;
@@ -46,6 +49,78 @@ tasksRouter.post("/api/addTask", async (req, res) => {
     todoList.todos.push(todo);
     await user.save();
     res.json(todoList.todos);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+//! UPDATE TASK
+// TODO: UPDATE TASK NOT TESTED YET
+tasksRouter.put("/api/updateTask", async (req, res) => {
+  try {
+    const { userId, todoId, todoTitle, todoDesc } = req.body;
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ msg: "User not found!" });
+    }
+    const todoList = await user.listOfTodos.find(
+      t => t._id.toString() === todoId
+    );
+    if (!todoList) {
+      return res.status(404).json({ msg: "TodoList not found!" });
+    }
+    const todo = todoList.todos.find(t => t._id.toString() === todoId);
+    if (!todo) {
+      return res.status(404).json({ msg: "Todo not found!" });
+    }
+    todo.todoTitle = todoTitle;
+    todo.todoDesc = todoDesc;
+    await user.save();
+    res.json(todoList.todos);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+//! DELETE TASK
+// TODO: DELETE TASK NOT TESTED YET
+tasksRouter.delete("/api/deleteTask", async (req, res) => {
+  try {
+    const { userId, todoId } = req.body;
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ msg: "User not found!" });
+    }
+    const todoList = await user.listOfTodos.find(
+      t => t._id.toString() === todoId
+    );
+    if (!todoList) {
+      return res.status(404).json({ msg: "TodoList not found!" });
+    }
+    const todoIndex = todoList.todos.findIndex(
+      t => t._id.toString() === todoId
+    );
+    if (todoIndex === -1) {
+      return res.status(404).json({ msg: "Todo not found!" });
+    }
+    todoList.todos.splice(todoIndex, 1);
+    await user.save();
+    res.json(todoList.todos);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+//! GET ALL TASKS
+// TODO: GET ALL TASKS NOT TESTED YET
+tasksRouter.get("/api/getTasks", async (req, res) => {
+  try {
+    const { userId } = req.body;
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ msg: "User not found!" });
+    }
+    res.json(user.listOfTodos);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
