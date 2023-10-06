4b21156cb5d078012a76e7127fba17d063a28350
Ibrahimfathi96 <himafathy796@gmail.com>
2023-10-06 16:02:04 +0300

    Retouches

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./reducres/authSlice";
-import todoSlice from "./reducres/TodoSlice";
+import todoReducer from "./reducres/TodoSlice";
 export default configureStore({
   reducer: {
     auth: authReducer,
-    todo: todoSlice
+    todo: todoReducer
   }
 });
